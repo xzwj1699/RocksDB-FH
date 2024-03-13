63e05b22478b5607cda8aa6ade5d9fc46c2e0def
xz1699 <xzwj@mail.ustc.edu.cn>
2024-03-13 18:40:03 +0800

    update script

--- ycsb/run_fh.sh
+++ ycsb/run_fh.sh
@@ -4,9 +4,11 @@
 workloads=("workloada.spec" "workloadb.spec" "workloadc.spec" "workloadd.spec" "workloade.spec" "workloadf.spec")
 # database data directory
 data_dir="/home/spdk/p4510/FH_Rocksdb_Data"
+# data_dir="/home/cqy/qemu-iso/zhangjuncheng.zjc/RocksDB_FH_Data"
 # data_dir="/home/xzwj1699/rocksdb/ycsb/rocksdb_data"
 # database log directory
 log_dir="/home/spdk/p4510/FH_Rocksdb_Data"
+# log_dir="/home/cqy/qemu-iso/zhangjuncheng.zjc/RocksDB_FH_Data"
 # log_dir="/home/xzwj1699/rocksdb/ycsb/rocksdb_data"
 # database name
 dbnames=("rocksdb")
@@ -33,6 +35,7 @@ request_num="20"
 req_size="4096"
 
 rocksdb_base="/home/spdk/p4510/rocksdb"
+rocksdb_base="/home/cqy/qemu-iso/zhangjuncheng.zjc/RocksDB_FH_Data_bak"
 # rocksdb_base="/home/xzwj1699/rocksdb/ycsb/rocksdb_data"
 for db in "${dbnames[@]}";
 do
